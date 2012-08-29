de7969131dc8ca790ee7603ca1c1dfd5caaae0b8
webmaster@deckersports.biz <webmaster@deckersports.biz>
2012-08-29 15:48:17 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webstore::Application.config.secret_token = 'd1a4dbadaadad59a99ddd70c3fcd4ec165ef6d683fb7c41497adb0d05a104d46369ad5a04713190d633a9c7e4606851a998b2672f1543eb214c5c43fb8c946b6'
