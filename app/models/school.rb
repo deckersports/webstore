844cc0258938efa14dd97b224f89f31a08bafa37
Matt <redleader36@bigmac.(none)>
2012-09-03 00:13:19 -0500

    More model fixes

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,7 @@ class School < ActiveRecord::Base
   attr_accessible :name, :mascot, :pri_color, :sec_color, :salesman, :address, :city, :zip, :abbr
 
 	has_many :store_infos
+	has_many :totals
   def to_label
     "#{name}"
   end
